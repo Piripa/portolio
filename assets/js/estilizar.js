88a2f2f67c6202888d43c10f0ff1a464acd424d6
Vinicius <viniaoliveira22@gmail.com>
2024-03-15 14:59:31 -0300

    atualização da página, inserção de bordas, criação da pagina contato

--- assets/js/estilizar.js
+++ assets/js/estilizar.js
@@ -7,22 +7,35 @@ function typeWriter(elemento) {
     });
 
 }
-function typeWriterArray(elemento,descricoes){
-        descricoes.forEach((descricao,i) => {
-            setTimeout(()=>{
-                elemento.innerHTML = descricao
-            }, 2000 * i)
-            
-        });
-    
-    
-       
+function typeWriterArray(elemento, descricoes) {
+    let i = 0;
+    let j = 0;
+    const interval = setInterval(() => {
+        if (i < descricoes.length) {
+            const descricao = descricoes[i];
+            const letras = descricao.split('');
+            if (j < letras.length) {
+                elemento.innerHTML += letras[j];
+                j++;
+            }
+            else {
+                elemento.innerHTML = '';
+                j = 0;
+                i++;
+            }
+        }
+        else {
+            clearInterval(interval);
+            typeWriterArray(elemento, descricoes);
+        }
+    }, 200);
 }
 
 
 
+
 const titulo = document.querySelector('h1');
-const descricoes = [' Vinicius Alessandro',' studying Computer Engineering',' Back-End Development']
+const descricoes = [' Vinicius Alessandro', ' studying Computer Engineering', ' Back-End Development']
 const texto = document.getElementById('textoAlternador')
 typeWriter(titulo);
-typeWriterArray(texto,descricoes)
+typeWriterArray(texto, descricoes)
